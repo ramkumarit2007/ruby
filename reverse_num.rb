83bf921631b6c30bd4cbde6c2251a07607fad8d9
Ramkumar Kuppuchamy <ramkumarit2007@gmail.com>
2014-08-14 15:45:40 +0530

    added one line if statement

--- reverse_num.rb
+++ reverse_num.rb
@@ -1,5 +1,8 @@
+#!/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/ruby
+
 puts "Enter the number to reverse"
 num = gets.chomp!
-puts num.to_s.reverse.to_i
 
-puts num.methods
+puts num.to_s.reverse.to_i if num!=""
+
+#puts num.methods
