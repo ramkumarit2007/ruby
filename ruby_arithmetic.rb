f0197fcff6682f6187b06038860d3d80ff647dcb
Ramkumar Kuppuchamy <ramkumarit2007@gmail.com>
2014-08-14 13:37:59 +0530

    Including one ruby in another ruby prgm and multiple argument methods

---
+++ ruby_arithmetic.rb
@@ -0,0 +1,20 @@
+
+def add(a,b)
+ puts "The given values are : #{a},#{b}"
+#a-b
+ a+b
+end
+
+def sub(a,b)
+ a-b
+end
+
+def mul(*num)
+ if num.count > 0
+    mul=1
+    num.each do |t|
+      mul *= t
+    end
+    puts "Multiply In arithmetic: #{mul}"
+ end
+end
---
+++ ruby_methods.rb
@@ -0,0 +1,6 @@
+#!/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/ruby
+load 'ruby_arithmetic.rb'
+#This method def accepts multiple inputs as array
+mul(1,2,3,4,5)
+
+puts "Addition: #{add(10,20)}"
