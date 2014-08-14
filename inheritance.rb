1682ef4077269ea9dcdfc49e3c1eccf479fe56dd
Ramkumar Kuppuchamy <ramkumarit2007@gmail.com>
2014-08-14 16:50:54 +0530

    added modules and load in classes

--- inheritance.rb
+++ inheritance.rb
@@ -1,4 +1,14 @@
 #!/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/ruby
+
+
+module Clueless
+ def test_module_method
+   puts "Testing the module method"
+ end
+ 
+load "ruby_arithmetic.rb"
+end
+
 class Area
    AUTHOR = "RAMKUMAR KUPPUCHAMY"
    
@@ -18,6 +28,7 @@ class Area
 end
 
 class Square < Area
+    include Clueless
     def getArea
 	@area = @width * @height
     end
@@ -30,7 +41,7 @@ class Rectangle < Area
 end
 
 class Triangle < Area
-   
+     include Clueless
      def initialize(a,b,c)
         @width,@height,@base = a,b,c
      end
@@ -43,10 +54,13 @@ end
 square_area = Square.new(10,10)
 rect_area = Rectangle.new(10,20)
 tri_area = Triangle.new(10,20,30)
-tri_area1 = Area.allocate
+#tri_area1 = Area.allocate
 
-puts "Allocated Triangle Area: #{tri_area1.getArea}"
+#puts "Allocated Triangle Area: #{tri_area1.getArea}"
 
 puts Area::AUTHOR
 
+puts square_area.test_module_method()
+puts tri_area.test_module_method()
+puts "multiply: #{mul(1,2,3)}"
 puts "Area of Square: #{square_area.getArea}\nArea of Rectangle: #{rect_area.getArea}\nArea of Triangle: #{tri_area.getArea}"
