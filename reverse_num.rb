0bdd85bed953833f648db0546e5f12474080d538
Ramkumar Kuppuchamy <ramkumarit2007@gmail.com>
2014-08-14 15:51:22 +0530

    added case statements example

--- reverse_num.rb
+++ reverse_num.rb
@@ -5,4 +5,21 @@ num = gets.chomp!
 
 puts num.to_s.reverse.to_i if num!=""
 
+unless num=="" 
+   puts num.to_s.reverse.to_i
+end
+
+age = 95
+
+case age
+when 0..11
+   puts "child" 
+when 15..19
+   puts "teen"
+when 20..29
+   puts "adult"
+else
+   puts "old"
+end
+
 #puts num.methods
