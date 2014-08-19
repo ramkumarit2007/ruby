f4be5ee03d0ee3c399f8c76bd9da3c6200e746b1
Ramkumar Kuppuchamy <ramkumarit2007@gmail.com>
2014-08-19 12:02:59 +0530

    working version with only one user option

--- mine_game.rb
+++ mine_game.rb
@@ -12,25 +12,37 @@ class MineGame
      end
      
      #Method to check for the mine or cell
-     def locate_mine(layout,option)
-        puts  "in locate mine ---- "
+     def locate_mine
         is_game = true
-        index1,index2 = option[2],option[4]
-        choose = option[0]
-        layout.each do | layoutVal |
-          layoutVal.each do | value |
-          if value != "f" or 0
-            if layout[index1][index2] == 'm' and choose == 'o'
+        cell_flagged  = false
+        index1,index2 = @option[2].to_i,@option[4].to_i
+        choose = @option[0]
+        @layout.each do | layoutVal |
+          layoutVal.split("").each do | value |
+            if @layout[index1][index2] == 'm' and choose == 'o'
               puts "Opened the mine -- You are OUT!!"
-             elsif layout[index1][index2] == 'x' and choose == 'o'
+              is_game = false
+             elsif @layout[index1][index2] == 'x' and choose == 'o'
                puts "You have opened the cell"
-               layout[index1][index2] = 0
-             else
-               
+               @layout[index1][index2] = '0'
+             elsif @layout[index1][index2] == 'm' and choose == 'f'
+               @layout[index1][index2] = 'f'
+             elsif @layout[index1][index2] == 'x' and choose == 'f'
+               puts "You have flagged the cell"
+               @layout[index1][index2] = 'f'
+               cell_flagged  = true
            end
-          else
-            
           end
-        
-     end 
+         end
+=begin       if value != "f" or value == '0' 
+
+          else
+            if cell_flagged
+               puts "You have flagged some cells, GAME OVER!!"
+            else
+               puts "Congrats!!! You have won the Game!"
+            end
+=end
+         @layout
+      end 
 end
--- mine_game_layout.rb
+++ mine_game_layout.rb
@@ -21,32 +21,6 @@ class MineLayout
   def getMineLayout
      @mineLayout.split(',')
   end
-  
-#Method to validate the given mine layout
-  def validate_layout(layout_grid)
-     #puts " Given Layout --- #{@mineLayout.split(',')}"
-     flag = true
-     layout_grid.each_with_index do | layout, index |
-        unless layout.length != layoutGrid[index-1].length 
-          #puts " #{layout.length} and #{layoutGrid[index-1].length} are same"
-        else
-          flag = false
-          raise "supply the valid mine layout"
-          break
-        end
-     end 
-  end 
-  
-#Method to print the layout
-  def print_layout(layout_grid)
-
-     puts "============================ LAYOUT =============================="
-     layout_grid.each do |layout|
-        puts "#{layout}\n"
-     end
-
-     puts "============================ LAYOUT =============================="
-  end  
 end
 
 if __FILE__ == $0
@@ -56,13 +30,14 @@ if __FILE__ == $0
   mine = MineLayout.new(MineGameLayout)
   validate_layout(mine.getMineLayout)
   print_layout(mine.getMineLayout)
-  
   puts "Enter option : "
   user_option = gets.chomp!
   puts user_option
+  is_game = true
   minegame = MineGame.new(mine.getMineLayout,user_option)
-  is_game =  minegame.locate_mine(mine.getMineLayout,user_option)
-  if is_game?
+  updated_layout = minegame.locate_mine
+  print_layout(updated_layout)
+  if is_game
     puts "you won the game"
   else
     puts "you are out!!"
