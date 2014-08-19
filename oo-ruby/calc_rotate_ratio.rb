cbf7a16b5b28ac7f11845d57bc69d6608e6ed427
Ramkumar Kuppuchamy <ramkumarit2007@gmail.com>
2014-08-19 19:25:37 +0530

    added logic to calc the ratio

--- oo-ruby/calc_rotate_ratio.rb
+++ oo-ruby/calc_rotate_ratio.rb
@@ -1,2 +1,11 @@
 #!/usr/local/rvm/rubies/ruby-1.9.3-p547/bin/ruby
+chainring = 57
+cog = 12
+big_gear_ratio = chainring / cog.to_f
+puts "Big Gear bicycle Ratio: #{big_gear_ratio}"
+
+chainring = 32
+cog = 26
+small_gear_ratio = chainring / cog.to_f
+puts "Small Gear bicycle Ratio: #{small_gear_ratio}"
 
