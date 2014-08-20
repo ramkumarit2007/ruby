dbcf498e87804a32c2006eae129ad2d26ae4bf03
Ramkumar Kuppuchamy <ramkumarit2007@gmail.com>
2014-08-20 17:59:51 +0530

    added defaults using fetch as well as hash merge

--- oo-ruby/manage_dependency_flexi_arguments.rb
+++ oo-ruby/manage_dependency_flexi_arguments.rb
@@ -6,16 +6,29 @@
 =begin
 1. This program examines, creating instances of objects in one another in the worst case where you are not able to move them ,even that time go for lazy loading initialization
 2. This uses hash as the intialization arguments - this removes the fixed argument order
+3. set defaults for the arguments (dont use || because for the boolean arguments that will not work expected, instead use 'fetch'
+4. make a separate method for the defaults when it gets complicated
 =end
 
 class Gear
 
      attr_reader :cog, :chainring, :rim, :tire
      def initialize(args)
+
+	#call defaults hash before intializing to set the missing keys
+	args = defaults.merge(args)
         @chainring = args[:chainring]
         @cog = args[:cog]
 	@rim = args[:rim]
 	@tire = args[:tire]
+ 	#@chainring = args.fetch(:chainring, 40)
+	#@cog = args.fetch(:cog,18)
+	#@rim = args.fetch(:rim,25)
+	#@tire = args.fetch(:tire,1.3)
+     end
+
+     def defaults	
+	{:chainring => 40, :cog => 18, :rim => 25, :tire => 1.3 }
      end
 
      def ratio
@@ -58,5 +71,7 @@ puts "Circumference ::: #{@wheel.circumference}"
 #puts "Big Gear ::: #{Gear.new(42,32,26,1.5).gear_inches}"
 #puts "Small Gear ::: #{Gear.new(42,32,24,1.25).gear_inches}"
 puts "Big Gear ::: #{Gear.new( :chainring => 42, :cog => 32, :rim => 26, :tire => 1.5).gear_inches}"
-puts "Small Gear ::: #{Gear.new( :chainring => 42, :cog => 32, :rim => 24, :tire => 1.25).gear_inches}"
+
+#The below initialization misses two arguments
+puts "Small Gear ::: #{Gear.new( :chainring => 42, :cog => 32).gear_inches}"
 
